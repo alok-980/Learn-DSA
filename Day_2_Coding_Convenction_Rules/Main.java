2b4f50c8cd24a01bbc5525d8440e77efbe7af173
Alok Chauhan <alokchauhan7896@gmail.com>
2026-08-03 09:26:46 +0530

    day 2 of dsa is completed

---
+++ Day_2_Coding_Convenction_Rules/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        int _age;
+        int $name;
+
+        int age_;
+        int name$;
+        int num4;
+        int count4num;
+        int num_var;
+        int m$m;
+        
+        System.out.println("hello");
+    }
+}
