afd0087a44f6e33c55dab1f3dfcd4db57f4fe3d0
Alok Chauhan <alokchauhan7896@gmail.com>
2026-08-03 09:08:30 +0530

    first commit

---
+++ Day_1_Comments_Variables/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]) {
+        // This is the comment in Java
+
+        int a = 20; // a is the variable in java
+
+        // System.out.println("Hello World"); // this syntax is for output in java
+
+        int age; // variable ka name likha and uska type bataya to yhi ho gyi - variable declaration
+        age = 60; // initilization - mtlb jab ham koi data first time variable me assigne / store kar dete hai
+
+        // int + int = int --> arithmatic operation
+        // String + Strin = String --> concatenation
+        // int + String = String --> concatenation
+
+        // 10 + 20 + 'hello' --> 30 hello
+        // 'hello' + 10 + 20 --> hellow 1020
+    }
+}
